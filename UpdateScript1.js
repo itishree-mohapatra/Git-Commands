8f1f3a962a7f3da9026710804a321dbfba809dd0
itishree-mohapatra <68557536+itishree-mohapatra@users.noreply.github.com>
2021-02-08 18:27:03 +0530

    Update UpdateScript1.js

--- UpdateScript1.js
+++ UpdateScript1.js
@@ -1,8 +1,9 @@
 var http=require("http");
 function onRequest(request,response)
 {
-response.weiteHead(200,{'content-Type':'text/plain'});
-response.write('Hello world');
-response.end();
+  response.weiteHead(200,{'content-Type':'text/plain'});
+  response.write('Hello world');
+  response.end();
+}
 
 http.createServer(onRequest).listen(8000);
